cc6e9fd60a2d9ca469d428c7fb89bcf09ef547f4
sanjay-xdr <sanjayjaiswal586@gmail.com>
2025-04-27 10:12:48 +0530

    adding cors

--- backend/app.js
+++ backend/app.js
@@ -5,8 +5,12 @@ const errorHandler = require('./middleware/errorHandler');
 
 const app = express();
 
-
-app.use(cors());
+const corsOptions = {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
